87a9700814768d25767193ee3b37b7a4d869e10b
Kamil Rejczyk <f1rejczyk@stud145.ftj.agh.edu.pl>
2012-11-30 18:53:34 +0100

    pierwszy komit

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void fun1();
+void fun2();
+void fun3();
+void fun4();
+int main(void){
+
+   return 0;
+}
+void fun1(){
+
+}
+void fun2(){
+
+}
+void fun3(){
+
+}
+void fun4(){
+
+}
